f8225ef80eb9f710bd240e816bf9ebf0d5fb72c8
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-12-24 16:10:42 +0530

    delete the core, pipeline and enroll

--- core/gait_encoder.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from core.gait_features import extract_frame_features
-
-def encode(sequence):
-    feats = np.array([extract_frame_features(kp) for kp in sequence])
-    return np.concatenate([feats.mean(axis=0), feats.std(axis=0)])
